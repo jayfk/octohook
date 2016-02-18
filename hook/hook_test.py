7318200b0eeecfabd03212d5553a444719a028d0
Jannis Gebauer <ja.geb@me.com>
2016-02-18 13:58:49 +0100

    init tests

---
+++ hook/hook_test.py
@@ -0,0 +1,21 @@
+import unittest
+
+
+class HookTestCase(unittest.TestCase):
+    pass
+
+
+class IsSignedTestCase(unittest.TestCase):
+    pass
+
+
+class ImportRepoByNameTestCase(unittest.TestCase):
+    pass
+
+
+class CheckEnvironmentTestCase(unittest.TestCase):
+    pass
+
+
+if __name__ == '__main__':
+    unittest.main()
